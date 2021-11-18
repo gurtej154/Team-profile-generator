c3456643dd8958a721589439ba199b54190bf6c4
Gurtej736l <gurtej154@gmail.com>
2021-11-18 11:33:30 +1030

    save changes to directory

--- src/TeamProfile.js
+++ src/TeamProfile.js
@@ -1,6 +1,6 @@
-const Manager = require("../lib/Manager");
-const Engineer = require("../lib/Engineer");
-const Intern = require("../lib/Intern");
+const Manager = require("../lib/manager");
+const Engineer = require("../lib/engineer");
+const Intern = require("../lib/intern");
 
 function generateCards(team) {
   let cards = [];
